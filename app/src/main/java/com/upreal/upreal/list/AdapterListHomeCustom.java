e56015e9a17672efccadac5619f54937249ffede
Elyo <sofiane.abbar.91@gmail.com>
2015-05-21 14:34:07 +0800

    List : Floating button added

--- app/src/main/java/com/upreal/upreal/list/AdapterListHomeCustom.java
+++ app/src/main/java/com/upreal/upreal/list/AdapterListHomeCustom.java
@@ -1,6 +1,5 @@
 package com.upreal.upreal.list;
 
-import android.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +9,16 @@ import android.widget.TextView;
 import com.upreal.upreal.R;
 
 /**
- * Created by Elyo on 11/05/2015.
+ * Created by Elyo on 18/05/2015.
  */
-public class AdapterListHome extends RecyclerView.Adapter<AdapterListHome.ViewHolder>{
+public class AdapterListHomeCustom  extends RecyclerView.Adapter<AdapterListHomeCustom.ViewHolder>{
 
-    private static final int TYPE_BASE = 0;
+    private static final int TYPE_HEADER = 0;
     private static final int TYPE_CUSTOM = 1;
 
     //private AlertDialog.Builder builder;
-    private String mBase_list[];
     private String mDelimiter[];
+    private String mListCust[];
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         int HolderId;
@@ -28,31 +27,31 @@ public class AdapterListHome extends RecyclerView.Adapter<AdapterListHome.ViewHo
         public ViewHolder(View itemView, int viewType) {
             super(itemView);
 
-            /*if (viewType == TYPE_CUSTOM) {
+            if (viewType == TYPE_CUSTOM) {
                 list_name = (TextView) itemView.findViewById(R.id.rowtextlist);
                 HolderId = 1;
-            } else */
-            if (viewType == TYPE_BASE){
+            } else
+            if (viewType == TYPE_HEADER){
                 list_name = (TextView) itemView.findViewById(R.id.rowtextlist);
                 HolderId = 0;
             }
         }
     }
 
-    AdapterListHome(String base_list[], String delimiter[]) {
-        this.mBase_list = base_list;
+    AdapterListHomeCustom(String[] list_cust, String delimiter[]) {
         this.mDelimiter = delimiter;
+        this.mListCust = list_cust;
     }
 
     @Override
-    public AdapterListHome.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        /*if (viewType == TYPE_CUSTOM) {
+    public AdapterListHomeCustom.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_CUSTOM) {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_home, parent, false);
             ViewHolder vhcust = new ViewHolder(v, viewType);
 
             return vhcust;
-        } else */
-        if (viewType == TYPE_BASE) {
+        } else
+        if (viewType == TYPE_HEADER) {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_home, parent, false);
             ViewHolder vhitem = new ViewHolder(v, viewType);
 
@@ -62,23 +61,21 @@ public class AdapterListHome extends RecyclerView.Adapter<AdapterListHome.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(AdapterListHome.ViewHolder holder, int position) {
+    public void onBindViewHolder(AdapterListHomeCustom.ViewHolder holder, int position) {
 //        holder.list_name.setText(mBase_list[position]);
-        /*if (holder.HolderId == 1) {
-            holder.list_name.setText(mDelimiter[0]);
-        } else */
+        if (holder.HolderId == 1) {
+            holder.list_name.setText(mListCust[position]);
+        } else
         if (holder.HolderId == 0) {
-            holder.list_name.setText(mBase_list[position]);
+            holder.list_name.setText(mDelimiter[0]);
         }
     }
 
     @Override
     public int getItemViewType(int position) {
         if (isPositionHeader(position))
-            return TYPE_BASE;
-
-        /*return TYPE_CUSTOM;*/
-        return TYPE_BASE;
+            return TYPE_HEADER;
+        return TYPE_CUSTOM;
     }
 
     private boolean isPositionHeader(int position) {
@@ -87,6 +84,6 @@ public class AdapterListHome extends RecyclerView.Adapter<AdapterListHome.ViewHo
 
     @Override
     public int getItemCount() {
-        return mBase_list.length;
+        return mListCust.length;
     }
 }
--- app/src/main/java/com/upreal/upreal/list/ListActivity.java
+++ app/src/main/java/com/upreal/upreal/list/ListActivity.java
@@ -1,29 +1,42 @@
 package com.upreal.upreal.list;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 
+import com.github.clans.fab.FloatingActionButton;
 import com.upreal.upreal.R;
 
 /**
  * Created by Elyo on 11/05/2015.
  */
-public class ListActivity extends ActionBarActivity {
+public class ListActivity extends ActionBarActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
+
+    //RecyclerView List Base
     private RecyclerView mRecyclerViewList;
     private RecyclerView.Adapter mAdapterList;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    //RecyclerView List Custom
+    private RecyclerView mRecyclerViewListCust;
+    private RecyclerView.Adapter mAdapterListCust;
+    private RecyclerView.LayoutManager mLayoutManagerCust;
+
+    private FloatingActionButton floatingButtonAddList;
+    private AlertDialog.Builder builder;
+
     private String base_list[];
     private String delimiter[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +53,16 @@ public class ListActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-/*        mRecyclerViewList = (RecyclerView) findViewById(R.id.recyclerlist);
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add new list");
+        floatingButtonAddList = (FloatingActionButton) findViewById(R.id.fabaddlist);
+        floatingButtonAddList.setOnClickListener(this);
+        mRecyclerViewList = (RecyclerView) findViewById(R.id.recyclerlist);
         mRecyclerViewList.setHasFixedSize(true);
         //mRecyclerViewList.setLayoutManager();
-
         mRecyclerViewList.setLayoutManager(new LinearLayoutManager(this));
-        mAdapterList = new AdapterListHome(base_list, delimiter);
+        mAdapterList = new AdapterListHomeBase(base_list, delimiter);
         mRecyclerViewList.setAdapter(mAdapterList);
-
         mRecyclerViewList.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
             @Override
             public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
@@ -58,6 +73,26 @@ public class ListActivity extends ActionBarActivity {
             public void onTouchEvent(RecyclerView rv, MotionEvent e) {
 
             }
-        });*/
+        });
+
+        String[] listCustom = new String[] {"list 1", "list 2", "list 3", "list 4", "list 5", "list 6"};
+
+        mRecyclerViewListCust = (RecyclerView) findViewById(R.id.recyclerlistCust);
+        mRecyclerViewListCust.setHasFixedSize(true);
+        //mRecyclerViewList.setLayoutManager();
+        mRecyclerViewListCust.setLayoutManager(new LinearLayoutManager(this));
+        mAdapterListCust = new AdapterListHomeCustom(listCustom, delimiter);
+        mRecyclerViewListCust.setAdapter(mAdapterListCust);
+    }
+
+    @Override
+    public void onClick(View v) {
+         switch (v.getId()) {
+             case R.id.fabaddlist:
+                 builder.create().show();
+                 return;
+             default:
+                 return;
+            }
     }
 }
