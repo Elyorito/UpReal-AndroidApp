65a9230a01acb5d10d648426501cbc9bd4ae0f15
Hiike <eric.ye@epitech.eu>
2015-08-18 17:06:32 +0200

    Recreate user package with the right name
    Plz work !

---
+++ app/src/main/java/com/upreal/upreal/user/UserActivity.java
@@ -0,0 +1,158 @@
+package com.upreal.upreal.user;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.upreal.upreal.R;
+import com.upreal.upreal.utils.Address;
+import com.upreal.upreal.utils.SessionManagerUser;
+import com.upreal.upreal.utils.SoapUserManager;
+import com.upreal.upreal.utils.SoapUserUtilManager;
+import com.upreal.upreal.utils.User;
+import com.upreal.upreal.view.SlidingTabLayout;
+
+/**
+ * Created by Eric on 26/05/2015.
+ */
+public class UserActivity extends ActionBarActivity {
+
+    private TextView userLocal;
+    private TextView userUsername;
+    private TextView userDesc;
+
+    private Address address;
+    private CharSequence title;
+    private User user;
+
+    private SessionManagerUser sessionManagerUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        userUsername = (TextView) findViewById(R.id.user_username);
+        userDesc = (TextView) findViewById(R.id.user_desc);
+        userLocal = (TextView) findViewById(R.id.user_local);
+
+        sessionManagerUser = new SessionManagerUser(getApplicationContext());
+        boolean toggleAccount = sessionManagerUser.isLogged();
+
+        if (getIntent().getExtras() == null)
+            user = sessionManagerUser.getUser();
+        else
+            user = getIntent().getExtras().getParcelable("listuser");
+
+        title = new String(user.getUsername());
+        userUsername.setText(user.getUsername());
+        /*
+        userLocal.setText("Adresse " + getResources().getString(R.string.not_defined));
+        if (user.getId_address() != -1 && user.getId_address() != 0)
+            new getAddress().execute(user.getId_address());
+        if (user.getFirstname() == null && user.getLastname() == null)
+            userDesc.setText(R.string.no_info);
+        else
+            userDesc.setText(user.getFirstname() + " " + user.getLastname());*/
+//        userLocal.setText(Need service get address from IdAddress);
+
+
+
+        CharSequence Tab[] = null;
+        if (toggleAccount && sessionManagerUser.getUserId() == user.getId()) {
+            // User
+            Tab = new CharSequence[]{getString(R.string.commentary), getString(R.string.options)};
+            Log.v("Options", "Here");
+        }
+        else {
+            // Other User
+            Tab = new CharSequence[] {getString(R.string.commentary), getString(R.string.social)};
+            Log.v("Social", "Here");
+            Log.v("session ID", String.valueOf(sessionManagerUser.getUserId()));
+            Log.v("user ID", String.valueOf(user.getId()));
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        UserViewPagerAdapter adapter = new UserViewPagerAdapter(getSupportFragmentManager(), Tab, 2, user, getApplicationContext());
+        mViewPager.setAdapter(adapter);
+
+        SlidingTabLayout mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tab);
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.ColorTabs);
+            }
+        });
+        mSlidingTabLayout.setViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings)
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class getAddress extends AsyncTask<Integer, Void, Address> {
+        SoapUserUtilManager pm = new SoapUserUtilManager();
+
+        @Override
+        protected Address doInBackground(Integer... params) {
+            return pm.getAddressInfo(params[0]);
+        }
+
+        protected void onPostExecute(Address result) {
+            userLocal.setText(result.getAddress() + "\n"
+                    + result.getAddress2() + "\n"
+                    + result.getCountry() + " "
+                    + result.getCity() + "\n"
+                    + ((result.getPostalCode() == 0) ? "" : String.valueOf(result.getPostalCode())));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new getUser().execute(user.getUsername());
+    }
+
+    private class getUser extends AsyncTask<String, Void, User> {
+        SoapUserManager um = new SoapUserManager();
+
+        @Override
+        protected User doInBackground(String... params) {
+            return um.getUserByUsername(params[0]);
+        }
+
+        protected void onPostExecute(User gUser) {
+            user.setUser(gUser);
+            sessionManagerUser.setUser(user);
+            userLocal.setText("Adresse " + getResources().getString(R.string.not_defined));
+            if (user.getId_address() != -1 && user.getId_address() != 0)
+                new getAddress().execute(user.getId_address());
+            if (user.getFirstname() == null && user.getLastname() == null)
+                userDesc.setText(R.string.no_info);
+            else
+                userDesc.setText(((user.getFirstname() == null) ? "Nom" : user.getFirstname()) + " " + ((user.getLastname() == null) ? "Prénom" : user.getLastname()));
+        }
+    }
+}
---
+++ app/src/main/java/com/upreal/upreal/user/UserSearchAdapter.java
@@ -0,0 +1,103 @@
+package com.upreal.upreal.user;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.upreal.upreal.R;
+import com.upreal.upreal.utils.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Eric on 26/05/2015.
+ */
+public class UserSearchAdapter extends RecyclerView.Adapter<UserSearchAdapter.ViewHolder> {
+
+/*
+    private String mProduct[];
+*/
+    private List<User> list = new ArrayList<User>();
+    private AlertDialog.Builder builder;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        int holderId;
+
+        TextView mNameUser;
+        CardView mCardview;
+        Button userUsername;
+        Button shareUser;
+
+        public ViewHolder(View itemView, int ViewType) {
+            super(itemView);
+
+            mCardview = (CardView) itemView.findViewById(R.id.cardview_search);
+/*            mNameProduct = (TextView) itemView.findViewById(R.id.but_cardview_go_product);*/
+            userUsername = (Button) itemView.findViewById(R.id.but_cardview_go_product);
+            shareUser = (Button) itemView.findViewById(R.id.but_cardview_share);
+        }
+    }
+
+    UserSearchAdapter(/*String product[]*/List<User> listuser) {
+        /*this.mProduct = product;*/
+
+        this.list = listuser;
+    }
+
+    @Override
+    public UserSearchAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_product_search, parent, false);
+        ViewHolder vhitem = new ViewHolder(v, viewType);
+
+        return vhitem;
+    }
+
+    @Override
+    public void onBindViewHolder(UserSearchAdapter.ViewHolder holder, final int position) {
+/*
+        holder.mNameProduct.setText(mProduct[position]);
+*/
+/*        holder.mNameProduct.setText(list.get(position).getName());*/
+        holder.userUsername.setText(list.get(position).getUsername());
+        holder.userUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.but_cardview_go_product:
+                        Toast.makeText(v.getContext(), "Fiche User :[" + list.get(position).getUsername() + "| " + Integer.toString(list.get(position).getId_address()) + "]", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(v.getContext(), UserActivity.class);
+                        intent.putExtra("listuser", list.get(position));
+                        v.getContext().startActivity(intent);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        holder.shareUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Partager [" + list.get(position).getUsername() + "] avec")
+                        .setView(R.layout.dialog_share)
+                        .create().show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        /*return mProduct.length;*/
+        return list.size();
+    }
+}
