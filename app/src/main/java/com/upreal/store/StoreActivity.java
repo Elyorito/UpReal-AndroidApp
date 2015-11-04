deb0628cd0a756d1fc0c4f2fd3d07305c3ba81c9
Kyosukke <kyosukke1@gmail.com>
2015-11-04 15:00:14 +0100

    Added Google Map on Store Activity

--- app/src/main/java/com/upreal/store/StoreActivity.java
+++ app/src/main/java/com/upreal/store/StoreActivity.java
@@ -5,12 +5,16 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 import com.upreal.R;
-import com.upreal.utils.BlurImages;
 import com.upreal.utils.CircleTransform;
 import com.upreal.utils.Store;
 
@@ -20,7 +24,7 @@ import com.upreal.utils.Store;
 public class StoreActivity extends AppCompatActivity implements View.OnClickListener {
 
     private CollapsingToolbarLayout collapsingToolbarLayout;
-    private ImageView imageBlurred;
+    private SupportMapFragment map;
     private ImageView imageStore;
     private TabLayout tabLayout;
 
@@ -32,16 +36,16 @@ public class StoreActivity extends AppCompatActivity implements View.OnClickList
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        setContentView(R.layout.activity_store);
 
         tabLayout = (TabLayout) findViewById(R.id.tabsproduct);
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        imageBlurred = (ImageView) findViewById(R.id.imageproductblurred);
         imageStore = (ImageView) findViewById(R.id.imageproduct);
         store = getIntent().getExtras().getParcelable("store");
 
+        createMapView();
+
         collapsingToolbarLayout.setTitle(store.getName());
-        Picasso.with(getApplicationContext()).load("http://163.5.84.202/Symfony/web/images/Store/" + store.getPicture()).transform(new BlurImages(getApplicationContext(), 25)).into(imageBlurred);
         Picasso.with(getApplicationContext()).load("http://163.5.84.202/Symfony/web/images/Store/" + store.getPicture()).transform(new CircleTransform()).into(imageStore);
         CharSequence tab[] = {"Info", "Produits", "Avis"};
 
@@ -49,6 +53,28 @@ public class StoreActivity extends AppCompatActivity implements View.OnClickList
         adapter = new StoreViewPagerAdapter(getSupportFragmentManager(), tab, 3, store, this);
         mViewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void createMapView() {
+        try {
+            if (map == null) {
+                map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_view));
+                if (map == null) {
+                    Toast.makeText(getApplicationContext(), "Error creating map", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (NullPointerException e) {
+            Log.e("mapApp", e.toString());
+        }
+    }
+
+    private void addMarker(String label, double latitude, double longitude) {
+        if (map != null) {
+            Log.e("StoreActivity", "Adding new marker " + label + ".");
+            map.getMap().addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(label).draggable(false));
+        } else {
+            Log.e("StoreActivity", "Map is null.");
+        }
     }
 
     @Override
